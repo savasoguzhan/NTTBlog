004ce5acedf2cf2dc0ea58907664457e6f1ac050
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-01-04 19:12:25 +0300

    added attribute

---
+++ NTTBlog.Service/Validation/AttributeValidator/ValidImageAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTTBlog.Service.Validation.AttributeValidator
+{
+    public  class ValidImageAttribute : ValidationAttribute
+    {
+        public int MaxFileLengthMb { get; set; }
+
+        public override bool IsValid(object? value)
+        {
+            IFormFile? file = value as IFormFile;
+
+            if (file == null)
+                return true;
+
+            if (MaxFileLengthMb > 0 && file.Length > MaxFileLengthMb * 1024 * 1024)
+            {
+                ErrorMessage = $"File Length greater than {MaxFileLengthMb} MB";
+                return false;
+            }
+            else if (!file.ContentType.StartsWith("image/"))
+            {
+                ErrorMessage = "Invalid Image Format";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ NTTBlog.Service/Validation/AttributeValidator/ValidPhoneAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTTBlog.Service.Validation.AttributeValidator
+{
+    public class ValidPhoneAttribute : ValidationAttribute
+    {
+
+        // For Turkish Number
+        public override bool IsValid(object? value)
+        {
+            string phoneNumber = value as string;
+
+            bool isNumeric = phoneNumber.All(char.IsDigit);
+
+            if (isNumeric)
+            {
+                // To have a valid phone number, it must be at least 10 digits long
+                if (phoneNumber.Length >= 10)
+                {
+                    // If the number is an international number, it should start with a country code, For Turkey it is 90.
+                    if (phoneNumber.StartsWith("905"))
+                    {
+                        // A valid Turkish phone number needs to have 11 digits
+                        if (phoneNumber.Length == 12)
+                        {
+                            return true;
+                        }
+                    }
+                    else if (phoneNumber.StartsWith("05"))
+                    {
+                        // A valid Turkish phone number needs to have 11 digits
+                        if (phoneNumber.Length == 11)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (phoneNumber.StartsWith("5"))
+                        {
+                            if (phoneNumber.Length == 10)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Return false if the number is invalid
+            ErrorMessage = $"Invalid phone number!";
+            return false;
+        }
+
+    }
+}
